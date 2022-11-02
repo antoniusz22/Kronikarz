3c84e5f00b3c754d3334457fdcf829789f8b5b5d
Antoni Zuber <antekzuber@gmail.com>
2022-11-02 11:53:56 +0100

    test

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ class Person {
 const persons = [
     new Person("Antoni", "Zuber", new Date("September 17, 2001"), new Date(Date.now()), "M", "None", "Kocham Ruch"),
     new Person("Rafał", "Ochorok", new Date("June 5, 2000"), new Date(Date.now()), "M", "COiG", ""),
-    new Person("Tomasz", "Barnaś", new Date("July 31, 1999"), new Date(Date.now()), "M", "TVP", "Tiktok: Tomasz_Barnas")
+    new Person("Tomasz", "Barnaś", new Date("July 31, 1999"), new Date(Date.now()), "M", "TVP", "Tiktok: Tomasz_Barnas"),
+    new Person("Michał", "Rojczyk", new Date("February 6, 2001"), new Date(Date.now()), "M", "Student", "Kozak Valo")
 ];
 console.log(persons);
