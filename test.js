2891228f337803ab9457efb409b795b26367df0c
Beruxik <57355176+Beruxik@users.noreply.github.com>
2022-12-29 12:48:48 +0100

    Początek frontendu

--- test.js
+++ test.js
@@ -1,145 +1,189 @@
-function Tree(
-  data,
-  {
-    // data is either tabular (array of objects) or hierarchy (nested objects)
-    path, // as an alternative to id and parentId, returns an array identifier, imputing internal nodes
-    id = Array.isArray(data) ? (d) => d.id : null, // if tabular data, given a d in data, returns a unique identifier (string)
-    parentId = Array.isArray(data) ? (d) => d.parentId : null, // if tabular data, given a node d, returns its parent’s identifier
-    children, // if hierarchical data, given a d in data, returns its children
-    tree = d3.tree, // layout algorithm (typically d3.tree or d3.cluster)
-    sort, // how to sort nodes prior to layout (e.g., (a, b) => d3.descending(a.height, b.height))
-    label, // given a node d, returns the display name
-    title, // given a node d, returns its hover text
-    link, // given a node d, its link (if any)
-    linkTarget = "_blank", // the target attribute for links (if any)
-    width = 640, // outer width, in pixels
-    height, // outer height, in pixels
-    r = 3, // radius of nodes
-    padding = 1, // horizontal padding for first and last column
-    fill = "#999", // fill for nodes
-    fillOpacity, // fill opacity for nodes
-    stroke = "#555", // stroke for links
-    strokeWidth = 1.5, // stroke width for links
-    strokeOpacity = 0.4, // stroke opacity for links
-    strokeLinejoin, // stroke line join for links
-    strokeLinecap, // stroke line cap for links
-    halo = "#fff", // color of label halo
-    haloWidth = 3, // padding around the labels
-    curve = d3.curveBumpX, // curve for the link
-  } = {}
-) {
-  // If id and parentId options are specified, or the path option, use d3.stratify
-  // to convert tabular data to a hierarchy; otherwise we assume that the data is
-  // specified as an object {children} with nested objects (a.k.a. the “flare.json”
-  // format), and use d3.hierarchy.
-  const root =
-    path != null
-      ? d3.stratify().path(path)(data)
-      : id != null || parentId != null
-      ? d3.stratify().id(id).parentId(parentId)(data)
-      : d3.hierarchy(data, children);
-
-  // Sort the nodes.
-  if (sort != null) root.sort(sort);
-
-  // Compute labels and titles.
-  const descendants = root.descendants();
-  const L = label == null ? null : descendants.map((d) => label(d.data, d));
-
-  // Compute the layout.
-  const dx = 10;
-  const dy = width / (root.height + padding);
-  tree().nodeSize([dx, dy])(root);
-
-  // Center the tree.
-  let x0 = Infinity;
-  let x1 = -x0;
-  root.each((d) => {
-    if (d.x > x1) x1 = d.x;
-    if (d.x < x0) x0 = d.x;
-  });
-
-  // Compute the default height.
-  if (height === undefined) height = x1 - x0 + dx * 2;
-
-  // Use the required curve
-  if (typeof curve !== "function") throw new Error(`Unsupported curve`);
-
-  const svg = d3
-    .create("svg")
-    .attr("viewBox", [(-dy * padding) / 2, x0 - dx, width, height])
-    .attr("width", width)
-    .attr("height", height)
-    .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
-    .attr("font-family", "sans-serif")
-    .attr("font-size", 10);
-
-  svg
-    .append("g")
-    .attr("fill", "none")
-    .attr("stroke", stroke)
-    .attr("stroke-opacity", strokeOpacity)
-    .attr("stroke-linecap", strokeLinecap)
-    .attr("stroke-linejoin", strokeLinejoin)
-    .attr("stroke-width", strokeWidth)
-    .selectAll("path")
-    .data(root.links())
-    .join("path")
-    .attr(
-      "d",
-      d3
-        .link(curve)
-        .x((d) => d.y)
-        .y((d) => d.x)
-    );
-
-  const node = svg
-    .append("g")
-    .selectAll("a")
-    .data(root.descendants())
-    .join("a")
-    .attr("xlink:href", link == null ? null : (d) => link(d.data, d))
-    .attr("target", link == null ? null : linkTarget)
-    .attr("transform", (d) => `translate(${d.y},${d.x})`);
-
-  node
-    .append("circle")
-    .attr("fill", (d) => (d.children ? stroke : fill))
-    .attr("r", r);
-
-  if (title != null) node.append("title").text((d) => title(d.data, d));
-
-  if (L)
-    node
-      .append("text")
-      .attr("dy", "0.32em")
-      .attr("x", (d) => (d.children ? -6 : 6))
-      .attr("text-anchor", (d) => (d.children ? "end" : "start"))
-      .attr("paint-order", "stroke")
-      .attr("stroke", halo)
-      .attr("stroke-width", haloWidth)
-      .text((d, i) => L[i]);
-
-  return svg.node();
+class Person {
+  constructor(
+    name,
+    surname,
+    dateOfBirth,
+    dateOfDeath,
+    placeOfBirth,
+    countryOfBirth,
+    sex,
+    occupation,
+    additionalInfo
+  ) {
+    this.name = name;
+    this.surname = surname;
+    this.dateOfBirth = dateOfBirth;
+    this.dateOfDeath = dateOfDeath;
+    this.placeOfBirth = placeOfBirth;
+    this.countryOfBirth = countryOfBirth;
+    this.age = Math.floor(
+      (Date.parse(dateOfDeath) - Date.parse(dateOfBirth)) / 31536000000,
+      0
+    ); // Amount of miliseconds in year
+    this.sex = sex;
+    this.occupation = occupation;
+    this.additionalInfo = additionalInfo;
+  }
 }
 
-flare = FileAttachment("flare.json").json();
+const persons = [
+  new Person(
+    "Antoni",
+    "Zuber",
+    new Date("September 17, 2001"),
+    new Date(Date.now()),
+    "Bielsko-Biała",
+    "Polska",
+    "M",
+    "Student",
+    "Kocham Ruch"
+  ),
+  new Person(
+    "Rafał",
+    "Ochorok",
+    new Date("June 5, 2000"),
+    new Date(Date.now()),
+    "Katowice",
+    "Polska",
+    "M",
+    "COiG",
+    ""
+  ),
+  new Person(
+    "Tomasz",
+    "Barnaś",
+    new Date("July 31, 1999"),
+    new Date(Date.now()),
+    "Siemianowice Śląskie",
+    "Polska",
+    "M",
+    "TVP",
+    "Tiktok: Tomasz_Barnas"
+  ),
+  new Person(
+    "Michał",
+    "Rojczyk",
+    new Date("February 6, 2001"),
+    new Date(Date.now()),
+    "Bielsko-Biała",
+    "Polska",
+    "M",
+    "Student",
+    "Kozak Valo"
+  ),
+];
 
-chart = Tree(flare, {
-  label: (d) => d.name,
-  title: (d, n) =>
-    `${n
-      .ancestors()
-      .reverse()
-      .map((d) => d.data.name)
-      .join(".")}`, // hover text
-  link: (d, n) =>
-    `https://github.com/prefuse/Flare/${
-      n.children ? "tree" : "blob"
-    }/master/flare/src/${n
-      .ancestors()
-      .reverse()
-      .map((d) => d.data.name)
-      .join("/")}${n.children ? "" : ".as"}`,
-  width: 1152,
-});
+const canvas = d3.select("#test");
+
+const x = 200;
+const y = 400;
+
+const svg = canvas.append("svg").attr("width", x).attr("height", y);
+
+const rect = svg
+  .append("rect")
+  .attr("stroke", "yellow")
+  .attr("fill", "white")
+  .attr("width", x)
+  .attr("height", y)
+  .attr("x", 0)
+  .attr("y", 0);
+
+const photo = svg
+  .append("image")
+  .attr("href", "test.jpg")
+  .attr("width", 180)
+  .attr("height", 200)
+  .attr("style", "margin-left: 10px");
+
+const name = svg
+  .append("text")
+  .text(`Imię: ${persons[0].name}`)
+  .attr("x", 0)
+  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 0}`)
+  .attr("dominant-baseline", "hanging")
+  .attr("text-anchor", "top-left");
+
+const surname = svg
+  .append("text")
+  .text(`Nazwisko: ${persons[0].surname}`)
+  .attr("x", 0)
+  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 1}`)
+  .attr("dominant-baseline", "hanging")
+  .attr("text-anchor", "top-left");
+
+const age = svg
+  .append("text")
+  .text(`Wiek: ${persons[0].age}`)
+  .attr("x", 0)
+  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 2}`)
+  .attr("dominant-baseline", "hanging")
+  .attr("text-anchor", "top-left");
+
+const dateOfBirth = svg
+  .append("text")
+  .text(
+    `Data urodzenia: ${persons[0].dateOfBirth.getDate()}/${
+      persons[0].dateOfBirth.getMonth() + 1 < 10
+        ? "0" + (persons[0].dateOfBirth.getMonth() + 1)
+        : persons[0].dateOfBirth.getMonth() + 1
+    }/${persons[0].dateOfBirth.getFullYear()}`
+  )
+  .attr("x", 0)
+  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 3}`)
+  .attr("dominant-baseline", "hanging")
+  .attr("text-anchor", "top-left");
+
+const dateOfDeath = svg
+  .append("text")
+  .text(
+    `Data śmierci: ${persons[0].dateOfDeath.getDate()}/${
+      persons[0].dateOfDeath.getMonth() + 1 < 10
+        ? "0" + (persons[0].dateOfDeath.getMonth() + 1)
+        : persons[0].dateOfDeath.getMonth() + 1
+    }/${persons[0].dateOfDeath.getFullYear()}`
+  )
+  .attr("x", 0)
+  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 4}`)
+  .attr("dominant-baseline", "hanging")
+  .attr("text-anchor", "top-left");
+
+const placeOfBirth = svg
+  .append("text")
+  .text(`Miejsce urodzenia: ${persons[0].placeOfBirth}`)
+  .attr("x", 0)
+  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 5}`)
+  .attr("dominant-baseline", "hanging")
+  .attr("text-anchor", "top-left");
+
+const countryOfBirth = svg
+  .append("text")
+  .text(`Kraj urodzenia: ${persons[0].countryOfBirth}`)
+  .attr("x", 0)
+  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 6}`)
+  .attr("dominant-baseline", "hanging")
+  .attr("text-anchor", "top-left");
+
+const sex = svg
+  .append("text")
+  .text(`Płeć: ${persons[0].sex}`)
+  .attr("x", 0)
+  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 7}`)
+  .attr("dominant-baseline", "hanging")
+  .attr("text-anchor", "top-left");
+
+const occupation = svg
+  .append("text")
+  .text(`Zawód: ${persons[0].occupation}`)
+  .attr("x", 0)
+  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 8}`)
+  .attr("dominant-baseline", "hanging")
+  .attr("text-anchor", "top-left");
+
+const additionalInfo = svg
+  .append("text")
+  .text(`Dodatkowe informacje: ${persons[0].additionalInfo}`)
+  .attr("x", 0)
+  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 9}`)
+  .attr("dominant-baseline", "hanging")
+  .attr("text-anchor", "top-left");
