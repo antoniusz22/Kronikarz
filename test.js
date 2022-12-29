0fffc2e88d7edfe14497e730b1a10a5f263db070
Beruxik <57355176+Beruxik@users.noreply.github.com>
2022-12-29 13:08:42 +0100

    Początek frontendu v2

--- test.js
+++ test.js
@@ -78,112 +78,117 @@ const canvas = d3.select("#test");
 const x = 200;
 const y = 400;
 
-const svg = canvas.append("svg").attr("width", x).attr("height", y);
+for (const person of persons) {
+  console.log(person);
+  const svg = canvas.append("svg").attr("width", x).attr("height", y);
 
-const rect = svg
-  .append("rect")
-  .attr("stroke", "yellow")
-  .attr("fill", "white")
-  .attr("width", x)
-  .attr("height", y)
-  .attr("x", 0)
-  .attr("y", 0);
+  const rect = svg
+    .append("rect")
+    .attr("stroke", "yellow")
+    .attr("fill", "white")
+    .attr("width", x)
+    .attr("height", y)
+    .attr("x", 0)
+    .attr("y", 0);
 
-const photo = svg
-  .append("image")
-  .attr("href", "test.jpg")
-  .attr("width", 180)
-  .attr("height", 200)
-  .attr("style", "margin-left: 10px");
+  const photo = svg
+    .append("image")
+    .attr("href", "test.jpg")
+    .attr("width", 180)
+    .attr("height", 200)
+    .attr("style", "margin-left: 10px");
 
-const name = svg
-  .append("text")
-  .text(`Imię: ${persons[0].name}`)
-  .attr("x", 0)
-  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 0}`)
-  .attr("dominant-baseline", "hanging")
-  .attr("text-anchor", "top-left");
+  const name = svg
+    .append("text")
+    .text(`Imię: ${person.name}`)
+    .attr("x", 0)
+    .attr("y", `${210 + (190 / Object.keys(person).length) * 0}`)
+    .attr("dominant-baseline", "hanging")
+    .attr("text-anchor", "top-left");
 
-const surname = svg
-  .append("text")
-  .text(`Nazwisko: ${persons[0].surname}`)
-  .attr("x", 0)
-  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 1}`)
-  .attr("dominant-baseline", "hanging")
-  .attr("text-anchor", "top-left");
+  const surname = svg
+    .append("text")
+    .text(`Nazwisko: ${person.surname}`)
+    .attr("x", 0)
+    .attr("y", `${210 + (190 / Object.keys(person).length) * 1}`)
+    .attr("dominant-baseline", "hanging")
+    .attr("text-anchor", "top-left");
 
-const age = svg
-  .append("text")
-  .text(`Wiek: ${persons[0].age}`)
-  .attr("x", 0)
-  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 2}`)
-  .attr("dominant-baseline", "hanging")
-  .attr("text-anchor", "top-left");
+  const age = svg
+    .append("text")
+    .text(`Wiek: ${person.age}`)
+    .attr("x", 0)
+    .attr("y", `${210 + (190 / Object.keys(person).length) * 2}`)
+    .attr("dominant-baseline", "hanging")
+    .attr("text-anchor", "top-left");
 
-const dateOfBirth = svg
-  .append("text")
-  .text(
-    `Data urodzenia: ${persons[0].dateOfBirth.getDate()}/${
-      persons[0].dateOfBirth.getMonth() + 1 < 10
-        ? "0" + (persons[0].dateOfBirth.getMonth() + 1)
-        : persons[0].dateOfBirth.getMonth() + 1
-    }/${persons[0].dateOfBirth.getFullYear()}`
-  )
-  .attr("x", 0)
-  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 3}`)
-  .attr("dominant-baseline", "hanging")
-  .attr("text-anchor", "top-left");
+  const dateOfBirth = svg
+    .append("text")
+    .text(
+      `Data urodzenia: ${person.dateOfBirth.getDate()}/${
+        person.dateOfBirth.getMonth() + 1 < 10
+          ? "0" + (person.dateOfBirth.getMonth() + 1)
+          : person.dateOfBirth.getMonth() + 1
+      }/${person.dateOfBirth.getFullYear()}`
+    )
+    .attr("x", 0)
+    .attr("y", `${210 + (190 / Object.keys(person).length) * 3}`)
+    .attr("dominant-baseline", "hanging")
+    .attr("text-anchor", "top-left");
 
-const dateOfDeath = svg
-  .append("text")
-  .text(
-    `Data śmierci: ${persons[0].dateOfDeath.getDate()}/${
-      persons[0].dateOfDeath.getMonth() + 1 < 10
-        ? "0" + (persons[0].dateOfDeath.getMonth() + 1)
-        : persons[0].dateOfDeath.getMonth() + 1
-    }/${persons[0].dateOfDeath.getFullYear()}`
-  )
-  .attr("x", 0)
-  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 4}`)
-  .attr("dominant-baseline", "hanging")
-  .attr("text-anchor", "top-left");
+  const dateOfDeath = svg
+    .append("text")
+    .text(
+      person.dateOfDeath.setHours(0,0,0,0) === new Date().setHours(0,0,0,0)
+        ? `Data śmierci: żyje`
+        : `Data śmierci: ${person.dateOfDeath.getDate()}/${
+            person.dateOfDeath.getMonth() + 1 < 10
+              ? "0" + (person.dateOfDeath.getMonth() + 1)
+              : person.dateOfDeath.getMonth() + 1
+          }/${person.dateOfDeath.getFullYear()}`
+    )
+    .attr("x", 0)
+    .attr("y", `${210 + (190 / Object.keys(person).length) * 4}`)
+    .attr("dominant-baseline", "hanging")
+    .attr("text-anchor", "top-left");
 
-const placeOfBirth = svg
-  .append("text")
-  .text(`Miejsce urodzenia: ${persons[0].placeOfBirth}`)
-  .attr("x", 0)
-  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 5}`)
-  .attr("dominant-baseline", "hanging")
-  .attr("text-anchor", "top-left");
+  const placeOfBirth = svg
+    .append("text")
+    .text(`Miejsce urodzenia: ${person.placeOfBirth}`)
+    .attr("x", 0)
+    .attr("y", `${210 + (190 / Object.keys(person).length) * 5}`)
+    .attr("dominant-baseline", "hanging")
+    .attr("text-anchor", "top-left");
 
-const countryOfBirth = svg
-  .append("text")
-  .text(`Kraj urodzenia: ${persons[0].countryOfBirth}`)
-  .attr("x", 0)
-  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 6}`)
-  .attr("dominant-baseline", "hanging")
-  .attr("text-anchor", "top-left");
+  const countryOfBirth = svg
+    .append("text")
+    .text(`Kraj urodzenia: ${person.countryOfBirth}`)
+    .attr("x", 0)
+    .attr("y", `${210 + (190 / Object.keys(person).length) * 6}`)
+    .attr("dominant-baseline", "hanging")
+    .attr("text-anchor", "top-left");
 
-const sex = svg
-  .append("text")
-  .text(`Płeć: ${persons[0].sex}`)
-  .attr("x", 0)
-  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 7}`)
-  .attr("dominant-baseline", "hanging")
-  .attr("text-anchor", "top-left");
+  const sex = svg
+    .append("text")
+    .text(`Płeć: ${person.sex}`)
+    .attr("x", 0)
+    .attr("y", `${210 + (190 / Object.keys(person).length) * 7}`)
+    .attr("dominant-baseline", "hanging")
+    .attr("text-anchor", "top-left");
 
-const occupation = svg
-  .append("text")
-  .text(`Zawód: ${persons[0].occupation}`)
-  .attr("x", 0)
-  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 8}`)
-  .attr("dominant-baseline", "hanging")
-  .attr("text-anchor", "top-left");
+  const occupation = svg
+    .append("text")
+    .text(`Zawód: ${person.occupation}`)
+    .attr("x", 0)
+    .attr("y", `${210 + (190 / Object.keys(person).length) * 8}`)
+    .attr("dominant-baseline", "hanging")
+    .attr("text-anchor", "top-left");
 
-const additionalInfo = svg
-  .append("text")
-  .text(`Dodatkowe informacje: ${persons[0].additionalInfo}`)
-  .attr("x", 0)
-  .attr("y", `${210 + (190 / Object.keys(persons[0]).length) * 9}`)
-  .attr("dominant-baseline", "hanging")
-  .attr("text-anchor", "top-left");
+  const additionalInfo = svg
+    .append("text")
+    .text(`Dodatkowe informacje: ${person.additionalInfo}`)
+    .attr("x", 0)
+    .attr("y", `${210 + (190 / Object.keys(person).length) * 9}`)
+    .attr("dominant-baseline", "hanging")
+    .attr("text-anchor", "top-left");
+}
