edcfb265011377ba2d2dd3ac6887c7222b3ad036
Beruxik <57355176+Beruxik@users.noreply.github.com>
2023-01-17 12:33:23 +0100

    Dodawanie funkcjonalności

--- fabric_example/index.js
+++ fabric_example/index.js
@@ -8,8 +8,24 @@ const initCanvas = (id) => {
 };
 
 const togglePan = () => {
-  if (currentMode === modes.pan) currentMode = "";
-  else currentMode = modes.pan;
+  if (currentMode === modes.pan) {
+    currentMode = "";
+
+    for (let object of canvas.getObjects()) {
+      if (object.get("type") !== "line") {
+        object.set({
+          selectable: true,
+        });
+      }
+    }
+  } else {
+    currentMode = modes.pan;
+    for (let object of canvas.getObjects()) {
+      object.set({
+        selectable: false,
+      });
+    }
+  }
 };
 
 const makeCanvasInteractive = (canvas) => {
@@ -19,8 +35,8 @@ const makeCanvasInteractive = (canvas) => {
     let delta = opt.e.deltaY;
     let zoom = canvas.getZoom();
     zoom *= 0.999 ** delta;
-    if (zoom > 20) zoom = 20;
-    if (zoom < 0.01) zoom = 0.01;
+    if (zoom > 3) zoom = 3;
+    if (zoom < 0.3) zoom = 0.3;
     canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
     opt.e.preventDefault();
     opt.e.stopPropagation();
@@ -53,7 +69,7 @@ const makeCanvasInteractive = (canvas) => {
   });
 };
 
-const createPerson = (canvas, person) => {
+const createPerson = (canvas, person, i) => {
   const rect = new fabric.Rect({
     left: 0,
     top: 0,
@@ -79,38 +95,123 @@ const createPerson = (canvas, person) => {
       top: 1,
     });
     let group = new fabric.Group([rect, img, text], {
-      left: 50,
-      top: 50,
+      id: `${person.name}_${person.surname}`,
+      left: 50 + i * 200,
+      top: 50 + i * 200,
+      spouse: `${person.spouse}`,
+      children: person.childs,
+      hasControls: false,
     });
     canvas.add(group);
+    canvas.renderAll();
+    group.on("moving", (opt) => {
+      let id = canvas.getActiveObject().id;
+      let linesParent = [];
+      let linesChild = [];
+      for (let object of canvas.getObjects()) {
+        if (new RegExp(`${id}:`).test(object.id)) {
+          linesParent.push(object);
+        } else if (new RegExp(`:${id}`).test(object.id)) {
+          linesChild.push(object);
+        }
+      }
+      for (let line of linesParent) {
+        line.set({
+          x1: line.x1 + opt.e.movementX / canvas.getZoom(),
+          y1: line.y1 + opt.e.movementY / canvas.getZoom(),
+        });
+      }
+      for (let line of linesChild) {
+        line.set({
+          x2: line.x2 + opt.e.movementX / canvas.getZoom(),
+          y2: line.y2 + opt.e.movementY / canvas.getZoom(),
+        });
+      }
+      canvas.renderAll();
+    });
+
+    group.on("mousedblclick", () => {
+      const modal = document.querySelector(`#${group.id}_dialog`);
+      modal.showModal();
+    });
   });
+};
+
+const makeLineBetweenChildAndParent = (parent, child) => {
+  let line = new fabric.Line(
+    [
+      parent.left + parent.width / 2,
+      parent.top + parent.height,
+      child.left + child.width / 2,
+      child.top,
+    ],
+    {
+      fill: "red",
+      stroke: "red",
+      strokeWidth: 2,
+      id: `${parent.id}:${child.id}`,
+      selectable: false,
+    }
+  );
+  canvas.add(line);
+  line.moveTo(0);
+  canvas.renderAll();
+};
+
+const makeLineBetweenSpouses = (husband, wife) => {
+  let line = new fabric.Line(
+    [
+      husband.left + husband.width,
+      husband.top + husband.height / 2,
+      wife.left,
+      wife.top + wife.height / 2,
+    ],
+    {
+      fill: "red",
+      stroke: "red",
+      strokeWidth: 2,
+      id: `${husband.id}:${wife.id}`,
+      selectable: false,
+    }
+  );
+  canvas.add(line);
+  line.moveTo(0);
+  canvas.renderAll();
 };
 
 const createDialog = (person) => {
   const dialog = document.createElement("dialog");
   let innerDiv = document.createElement("div");
-  innerDiv.innerHTML += `Wiek: ${persons[0].age} <br />`;
-  innerDiv.innerHTML += `Data urodzenia: ${persons[0].dateOfBirth.getDate()}/${
-    persons[0].dateOfBirth.getMonth() + 1 < 10
-      ? "0" + (persons[0].dateOfBirth.getMonth() + 1)
-      : persons[0].dateOfBirth.getMonth() + 1
-  }/${persons[0].dateOfBirth.getFullYear()} <br />`;
+  innerDiv.innerHTML += `Imię: ${person.name} <br />`;
+  innerDiv.innerHTML += `Nazwisko: ${person.surname} <br />`;
+  innerDiv.innerHTML += `Wiek: ${person.age} <br />`;
+  innerDiv.innerHTML += `Data urodzenia: ${person.dateOfBirth.getDate()}/${
+    person.dateOfBirth.getMonth() + 1 < 10
+      ? "0" + (person.dateOfBirth.getMonth() + 1)
+      : person.dateOfBirth.getMonth() + 1
+  }/${person.dateOfBirth.getFullYear()} <br />`;
   innerDiv.innerHTML +=
-    persons[0].dateOfDeath.setHours(0, 0, 0, 0) ===
-    new Date().setHours(0, 0, 0, 0)
-      ? `Data śmierci: żyje <br />`
-      : `Data śmierci: ${persons[0].dateOfDeath.getDate()}/${
-          persons[0].dateOfDeath.getMonth() + 1 < 10
-            ? "0" + (persons[0].dateOfDeath.getMonth() + 1)
-            : persons[0].dateOfDeath.getMonth() + 1
-        }/${persons[0].dateOfDeath.getFullYear()} <br />`;
-  innerDiv.innerHTML += `Miejsce urodzenia: ${persons[0].placeOfBirth} <br />`;
-  innerDiv.innerHTML += `Kraj urodzenia: ${persons[0].countryOfBirth} <br />`;
-  innerDiv.innerHTML += `Płeć: ${persons[0].sex} <br />`;
-  innerDiv.innerHTML += `Zawód: ${persons[0].occupation} <br />`;
-  innerDiv.innerHTML += `${persons[0].additionalInfo}`;
+    person.dateOfDeath.setHours(0, 0, 0, 0) === new Date().setHours(0, 0, 0, 0)
+      ? ``
+      : `Data śmierci: ${person.dateOfDeath.getDate()}/${
+          person.dateOfDeath.getMonth() + 1 < 10
+            ? "0" + (person.dateOfDeath.getMonth() + 1)
+            : person.dateOfDeath.getMonth() + 1
+        }/${person.dateOfDeath.getFullYear()} <br />`;
+  innerDiv.innerHTML += `Miejsce urodzenia: ${person.placeOfBirth} <br />`;
+  innerDiv.innerHTML += `Kraj urodzenia: ${person.countryOfBirth} <br />`;
+  innerDiv.innerHTML += `Płeć: ${person.sex} <br />`;
+  innerDiv.innerHTML += `Zawód: ${person.occupation} <br />`;
+  innerDiv.innerHTML += `${person.additionalInfo}`;
+  dialog.setAttribute("id", `${person.name}_${person.surname}_dialog`);
   dialog.appendChild(innerDiv);
   document.body.appendChild(dialog);
+};
+
+const getObject = (id) => {
+  for (let object of canvas.getObjects()) {
+    if (object.id === id) return object;
+  }
 };
 
 const canvas = initCanvas("canvas");
@@ -124,11 +225,61 @@ const modes = {
 const togglePanButton = document.querySelector("#togglePan");
 togglePanButton.addEventListener("mousedown", togglePan);
 
+const resetZoomButton = document.querySelector("#resetZoom");
+resetZoomButton.addEventListener("mousedown", () => {
+  canvas.zoomToPoint(new fabric.Point(0, 0), 1);
+});
+
+const getCoords = document.querySelector("#getCoords");
+getCoords.addEventListener("mousedown", () => {
+  let o1 = getObject("Tomasz_Barnaś");
+  let o2 = getObject("Antoni_Zuber");
+  let o3 = getObject("Rafał_Ochorok");
+  console.log(o2.children);
+  makeLineBetweenSpouses(o2, getObject(o2.spouse));
+  for (let child of o2.children) {
+    console.log(child);
+    makeLineBetweenChildAndParent(o2, getObject(child));
+    makeLineBetweenChildAndParent(getObject(o2.spouse), getObject(child));
+  }
+  canvas.renderAll();
+});
+
 makeCanvasInteractive(canvas);
 
-for (let person of persons) {
-  createPerson(canvas, person);
+for (const [index, person] of persons.entries()) {
+  createPerson(canvas, person, index);
   createDialog(person);
 }
 
 canvas.renderAll();
+
+const modal = document.querySelector("#addUserForm");
+const openModal = document.querySelector(".openAddUserForm-btn");
+const closeModal = document.querySelector(".closeAddUserForm-btn");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
+
+let cancel = true;
+window.addEventListener("mousemove", () => {
+  if (cancel) {
+    let o1 = getObject("Tomasz_Barnaś");
+    let o2 = getObject("Antoni_Zuber");
+    let o3 = getObject("Rafał_Ochorok");
+    console.log(o2.children);
+    makeLineBetweenSpouses(o2, getObject(o2.spouse));
+    for (let child of o2.children) {
+      console.log(child);
+      makeLineBetweenChildAndParent(o2, getObject(child));
+      makeLineBetweenChildAndParent(getObject(o2.spouse), getObject(child));
+    }
+    canvas.renderAll();
+  }
+  cancel = false;
+});
--- fabric_example/test.js
+++ fabric_example/test.js
@@ -4,16 +4,20 @@ export class Person {
     surname,
     dateOfBirth,
     dateOfDeath,
+    dateOfMarriage,
     placeOfBirth,
     countryOfBirth,
     sex,
     occupation,
-    additionalInfo
+    additionalInfo,
+    spouse,
+    childs
   ) {
     this.name = name;
     this.surname = surname;
     this.dateOfBirth = dateOfBirth;
     this.dateOfDeath = dateOfDeath;
+    this.dateOfMarriage = dateOfMarriage;
     this.placeOfBirth = placeOfBirth;
     this.countryOfBirth = countryOfBirth;
     this.age = Math.floor(
@@ -23,6 +27,8 @@ export class Person {
     this.sex = sex;
     this.occupation = occupation;
     this.additionalInfo = additionalInfo;
+    this.spouse = spouse;
+    this.childs = childs;
   }
 }
 
@@ -32,16 +38,20 @@ export const persons = [
     "Zuber",
     new Date("September 17, 2001"),
     new Date(Date.now()),
+    new Date("June 1, 2022"),
     "Bielsko-Biała",
     "Polska",
     "M",
     "Student",
-    "Kocham Ruch"
+    "Kocham Ruch",
+    "Natalia_Staniek",
+    ["Cristiano_Zuber", "Wilhelm_Zuber"]
   ),
   new Person(
     "Rafał",
     "Ochorok",
     new Date("June 5, 2000"),
+    new Date(Date.now()),
     new Date(Date.now()),
     "Katowice",
     "Polska",
@@ -54,6 +64,7 @@ export const persons = [
     "Barnaś",
     new Date("July 31, 1999"),
     new Date(Date.now()),
+    new Date(Date.now()),
     "Siemianowice Śląskie",
     "Polska",
     "M",
@@ -64,6 +75,7 @@ export const persons = [
     "Michał",
     "Rojczyk",
     new Date("February 6, 2001"),
+    new Date("February 6, 2010"),
     new Date(Date.now()),
     "Bielsko-Biała",
     "Polska",
@@ -71,234 +83,46 @@ export const persons = [
     "Student",
     "Kozak Valo"
   ),
+  new Person(
+    "Natalia",
+    "Staniek",
+    new Date("December 17, 2002"),
+    new Date(Date.now()),
+    new Date("June 1, 2022"),
+    "Bielsko-Biała",
+    "Polska",
+    "K",
+    "Student",
+    "Kocham Antka",
+    "Antoni_Zuber",
+    ["Cristiano_Zuber"]
+  ),
+  new Person(
+    "Cristiano",
+    "Zuber",
+    new Date("January 17, 2023"),
+    new Date(Date.now()),
+    new Date(Date.now()),
+    "Bielsko-Biała",
+    "Polska",
+    "M",
+    "Student",
+    "Kocham Ruch",
+    "",
+    []
+  ),
+  new Person(
+    "Wilhelm",
+    "Zuber",
+    new Date("January 17, 2023"),
+    new Date(Date.now()),
+    new Date(Date.now()),
+    "Bielsko-Biała",
+    "Polska",
+    "M",
+    "Student",
+    "Kocham Ruch",
+    "Natalia_Staniek",
+    ["Cristiano_Zuber", "Wilhelm_Zuber"]
+  ),
 ];
-
-// const canvas = d3.select(".person");
-
-// const x = 200;
-// const y = 200;
-
-// let div = document.querySelector(".person");
-
-// canvas
-//   .append("img")
-//   .attr("src", "test.jpg")
-//   .attr("width", "150px")
-//   .attr("height", "180px");
-
-// div.innerHTML += `${persons[0].name} ${persons[0].surname}`;
-// let innerDiv = document.createElement("div");
-// innerDiv.innerHTML += `Wiek: ${persons[0].age} <br />`;
-// innerDiv.innerHTML += `Data urodzenia: ${persons[0].dateOfBirth.getDate()}/${
-//   persons[0].dateOfBirth.getMonth() + 1 < 10
-//     ? "0" + (persons[0].dateOfBirth.getMonth() + 1)
-//     : persons[0].dateOfBirth.getMonth() + 1
-// }/${persons[0].dateOfBirth.getFullYear()} <br />`;
-// innerDiv.innerHTML +=
-//   persons[0].dateOfDeath.setHours(0, 0, 0, 0) ===
-//   new Date().setHours(0, 0, 0, 0)
-//     ? `Data śmierci: żyje <br />`
-//     : `Data śmierci: ${persons[0].dateOfDeath.getDate()}/${
-//         persons[0].dateOfDeath.getMonth() + 1 < 10
-//           ? "0" + (persons[0].dateOfDeath.getMonth() + 1)
-//           : persons[0].dateOfDeath.getMonth() + 1
-//       }/${persons[0].dateOfDeath.getFullYear()} <br />`;
-// innerDiv.innerHTML += `Miejsce urodzenia: ${persons[0].placeOfBirth} <br />`;
-// innerDiv.innerHTML += `Kraj urodzenia: ${persons[0].countryOfBirth} <br />`;
-// innerDiv.innerHTML += `Płeć: ${persons[0].sex} <br />`;
-// innerDiv.innerHTML += `Zawód: ${persons[0].occupation} <br />`;
-// innerDiv.innerHTML += `${persons[0].additionalInfo}`;
-// div.appendChild(innerDiv);
-
-
-
-
-// canvas
-//   .html(`${persons[0].name} ${persons[0].surname}`)
-//   .append("div")
-//   .text(`Wiek: ${persons[0].age} <br />`)
-//   .text(
-//     `Data urodzenia: ${persons[0].dateOfBirth.getDate()}/${
-//       persons[0].dateOfBirth.getMonth() + 1 < 10
-//         ? "0" + (persons[0].dateOfBirth.getMonth() + 1)
-//         : persons[0].dateOfBirth.getMonth() + 1
-//     }/${persons[0].dateOfBirth.getFullYear()} <br />`
-//   )
-//   .html(
-//     persons[0].dateOfDeath.setHours(0, 0, 0, 0) ===
-//       new Date().setHours(0, 0, 0, 0)
-//       ? `Data śmierci: żyje`
-//       : `Data śmierci: ${persons[0].dateOfDeath.getDate()}/${
-//           persons[0].dateOfDeath.getMonth() + 1 < 10
-//             ? "0" + (persons[0].dateOfDeath.getMonth() + 1)
-//             : persons[0].dateOfDeath.getMonth() + 1
-//         }/${persons[0].dateOfDeath.getFullYear()}`
-//   )
-//   .html(`Miejsce urodzenia: ${persons[0].placeOfBirth} <br />`)
-//   .html(`Kraj urodzenia: ${persons[0].countryOfBirth} <br />`)
-//   .html(`Płeć: ${persons[0].sex} <br />`)
-//   .html(`Zawód: ${persons[0].occupation} <br />`)
-//   .html(`${persons[0].additionalInfo}`);
-
-// canvas.html(`${persons[0].name} ${persons[0].surname}`).append("div")
-//   .html(`Wiek: ${persons[0].age} <br />
-// Data urodzenia: ${persons[0].dateOfBirth.getDate()}/${
-//   persons[0].dateOfBirth.getMonth() + 1 < 10
-//     ? "0" + (persons[0].dateOfBirth.getMonth() + 1)
-//     : persons[0].dateOfBirth.getMonth() + 1
-// }/${persons[0].dateOfBirth.getFullYear()} <br />
-// Data śmierci: ${person.dateOfDeath.setHours(0, 0, 0, 0) === new Date().setHours(0, 0, 0, 0)
-//   ? `Data śmierci: żyje`
-//   : `Data śmierci: ${person.dateOfDeath.getDate()}/${
-//     person.dateOfDeath.getMonth() + 1 < 10
-//       ? "0" + (person.dateOfDeath.getMonth() + 1)
-//       : person.dateOfDeath.getMonth() + 1
-//   }/${person.dateOfDeath.getFullYear()}} <br />
-// Miejsce urodzenia: ${persons[0].placeOfBirth} <br />
-// Kraj urodzenia: ${persons[0].countryOfBirth} <br />
-// Płeć: ${persons[0].sex} <br />
-// Zawód: ${persons[0].occupation} <br />
-// ${persons[0].additionalInfo}`);
-
-// for (const person of persons) {
-//   // console.log(person);
-//   const svg = canvas
-//     .append("svg")
-//     .attr("width", x)
-//     .attr("height", y)
-//     .attr("id", `${person.name}_${person.surname}`)
-//     .attr("class", "expand_on_hover")
-//     .attr("onclick", "this.style.height = '400px';");
-//   // .attr("style", "position: absolute;");
-
-//   const rect = svg
-//     .append("rect")
-//     .attr("stroke", "yellow")
-//     .attr("fill", "white")
-//     .attr("width", x)
-//     .attr("height", y)
-//     .attr("x", 0)
-//     .attr("y", 0);
-
-//   const photo = svg
-//     .append("image")
-//     .attr("href", "test.jpg")
-//     .attr("width", 180)
-//     .attr("height", 150)
-//     .attr("style", "margin-left: 10px");
-
-//   const name_surname = svg
-//     .append("text")
-//     .text(`${person.name} ${person.surname}`)
-//     .attr("y", 160)
-//     .attr("class", "name_surname");
-
-//   const name = svg
-//     .append("text")
-//     .text(`Imię: ${person.name}`)
-//     .attr("x", 0)
-//     .attr("y", `${210 + (190 / Object.keys(person).length) * 0}`)
-//     .attr("dominant-baseline", "hanging")
-//     .attr("text-anchor", "top-left")
-//     .attr("class", "show_on_hover");
-
-//   const surname = svg
-//     .append("text")
-//     .text(`Nazwisko: ${person.surname}`)
-//     .attr("x", 0)
-//     .attr("y", `${210 + (190 / Object.keys(person).length) * 1}`)
-//     .attr("dominant-baseline", "hanging")
-//     .attr("text-anchor", "top-left")
-//     .attr("class", "show_on_hover");
-
-//   const age = svg
-//     .append("text")
-//     .text(`Wiek: ${person.age}`)
-//     .attr("x", 0)
-//     .attr("y", `${210 + (190 / Object.keys(person).length) * 2}`)
-//     .attr("dominant-baseline", "hanging")
-//     .attr("text-anchor", "top-left")
-//     .attr("class", "show_on_hover");
-
-//   const dateOfBirth = svg
-//     .append("text")
-//     .text(
-//       `Data urodzenia: ${person.dateOfBirth.getDate()}/${
-//         person.dateOfBirth.getMonth() + 1 < 10
-//           ? "0" + (person.dateOfBirth.getMonth() + 1)
-//           : person.dateOfBirth.getMonth() + 1
-//       }/${person.dateOfBirth.getFullYear()}`
-//     )
-//     .attr("x", 0)
-//     .attr("y", `${210 + (190 / Object.keys(person).length) * 3}`)
-//     .attr("dominant-baseline", "hanging")
-//     .attr("text-anchor", "top-left")
-//     .attr("class", "show_on_hover");
-
-// const dateOfDeath = svg
-//   .append("text")
-//   .text(
-//     person.dateOfDeath.setHours(0, 0, 0, 0) === new Date().setHours(0, 0, 0, 0)
-//       ? `Data śmierci: żyje`
-//       : `Data śmierci: ${person.dateOfDeath.getDate()}/${
-//           person.dateOfDeath.getMonth() + 1 < 10
-//             ? "0" + (person.dateOfDeath.getMonth() + 1)
-//             : person.dateOfDeath.getMonth() + 1
-//         }/${person.dateOfDeath.getFullYear()}`
-//   )
-//   .attr("x", 0)
-//   .attr("y", `${210 + (190 / Object.keys(person).length) * 4}`)
-//   .attr("dominant-baseline", "hanging")
-//   .attr("text-anchor", "top-left")
-//   .attr("class", "show_on_hover");
-
-//   const placeOfBirth = svg
-//     .append("text")
-//     .text(`Miejsce urodzenia: ${person.placeOfBirth}`)
-//     .attr("x", 0)
-//     .attr("y", `${210 + (190 / Object.keys(person).length) * 5}`)
-//     .attr("dominant-baseline", "hanging")
-//     .attr("text-anchor", "top-left")
-//     .attr("class", "show_on_hover");
-
-//   const countryOfBirth = svg
-//     .append("text")
-//     .text(`Kraj urodzenia: ${person.countryOfBirth}`)
-//     .attr("x", 0)
-//     .attr("y", `${210 + (190 / Object.keys(person).length) * 6}`)
-//     .attr("dominant-baseline", "hanging")
-//     .attr("text-anchor", "top-left")
-//     .attr("class", "show_on_hover");
-
-//   const sex = svg
-//     .append("text")
-//     .text(`Płeć: ${person.sex}`)
-//     .attr("x", 0)
-//     .attr("y", `${210 + (190 / Object.keys(person).length) * 7}`)
-//     .attr("dominant-baseline", "hanging")
-//     .attr("text-anchor", "top-left")
-//     .attr("class", "show_on_hover");
-
-//   const occupation = svg
-//     .append("text")
-//     .text(`Zawód: ${person.occupation}`)
-//     .attr("x", 0)
-//     .attr("y", `${210 + (190 / Object.keys(person).length) * 8}`)
-//     .attr("dominant-baseline", "hanging")
-//     .attr("text-anchor", "top-left")
-//     .attr("class", "show_on_hover");
-
-//   const additionalInfo = svg
-//     .append("text")
-//     .text(`Dodatkowe informacje: ${person.additionalInfo}`)
-//     .attr("x", 0)
-//     .attr("y", `${210 + (190 / Object.keys(person).length) * 9}`)
-//     .attr("dominant-baseline", "hanging")
-//     .attr("text-anchor", "top-left")
-//     .attr("class", "show_on_hover");
-// }
-
-// console.log(
-//   document.querySelector("#Antoni_Zuber").getBoundingClientRect().x +
-//     document.querySelector("#Antoni_Zuber").getBoundingClientRect().width / 2
-// );
