be0639e48f9f0e8f8b666d8152b6aa098bfe5d99
rafaello166 <rafaello166@interia.pl>
2023-01-18 19:48:50 +0100

    html date

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
@@ -19,15 +20,19 @@ class UserType extends AbstractType
             ->add('lastName')
 //            ->add('email', EmailType::class)
 //            ->add('telephone', TelType::class)
-            ->add('birthday')
-            ->add('death')
+            ->add('birthday', DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('death', DateType::class, [
+                'widget' => 'single_text',
+                'required' => false,
+            ])
             ->add('birthplace')
             ->add('country_of_birth')
             ->add('sex')
             ->add('profession')
             ->add('additional_information')
-            ->add('Submit', SubmitType::class)
-        ;
+            ->add('Submit', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
